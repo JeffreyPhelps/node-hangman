c7ba39a4c00066c38107305732353c045585951a
Jeffrey Phelps <phelpsjeffrey@yahoo.com>
2018-02-04 17:05:11 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-// Jeffrey Phelps - DU Web Dev Bootcamp 2017/2018 - Week-10 Homework (Node Liri Bot)
+// Jeffrey Phelps - DU Web Dev Bootcamp 2017/2018 - Mod-11 Homework (Node Liri Bot)
 
 //list of words to guess.
 exports.newWord = {
-  wordList:['SUPERMAN','BATMAN','WONDER WOMAN','THE FLASH','AQUAMAN','CYBORG','BATGIRL','ROBIN','NIGHTWING','BLACK CANARY','HAWKGIRL','GREEN ARROW','SUPERGIRL'],
+  wordList: ['TOM BRADY','JOE MONTANA','TERRY BRADSHAW','TROY AIKMAN','PEYTON MANNING','ELI MANNING','JIM PLUNKETT','BOB GRIESE','ROGER STAUBACH','BEN ROETHLISBERGER','JOHN ELWAY','BART STARR'],
   hangman: ["\n\n\n\n\n\n\n-------------", "\n |\n |\n |\n |\n |\n |\n |\n-------------", "____________\n |         |\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        |\n |\n-------------","____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        | |\n |\n-------------"]
 };
